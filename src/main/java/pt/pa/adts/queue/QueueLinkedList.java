8d75a01598bf4623fce378828ad517f1d7be8fbd
rafaeljunqueira2024151531 <2024151531@estudantes.ips.pt>
2025-10-01 11:17:49 +0100

    QueueLinkedList

--- src/main/java/pt/pa/adts/queue/QueueLinkedList.java
+++ src/main/java/pt/pa/adts/queue/QueueLinkedList.java
@@ -4,16 +4,79 @@ package pt.pa.adts.queue;
  * This class must implement the Queue interface
  * @param <T>
  */
-public class QueueLinkedList<T>  {
+public class QueueLinkedList<T> implements Queue<T> {
 
     private ListNode header, trailer;
     private int size;
 
     public QueueLinkedList() {
-        //TODO: construtor deve inicializar uma fila vazia
+        this.trailer = null;
+        this.size = 0;
+        this.header = null;
     }
 
-    //TODO: implementar métodos da interface à custa da estrutura de dados fornecida
+    @Override
+    public void enqueue(T elem) throws QueueFullException, NullPointerException {
+        if (elem == null) {
+            throw new NullPointerException("Element cannot be null.");
+        }
+
+        ListNode newNode = new ListNode(elem, trailer, null);
+
+        if (isEmpty()) {
+            header = newNode;
+        } else {
+            trailer.next = newNode;
+        }
+
+        trailer = newNode;
+        size++;
+    }
+
+    @Override
+    public T dequeue() throws QueueEmptyException {
+        if (isEmpty()) {
+            throw new QueueEmptyException("Queue is empty.");
+        }
+
+        T element = header.element;
+        header = header.next;
+
+        if (header != null) {
+            header.prev = null;
+        } else {
+            // Fila ficou vazia
+            trailer = null;
+        }
+
+        size--;
+        return element;
+    }
+
+    @Override
+    public T front() throws QueueEmptyException {
+        if (isEmpty()) {
+            throw new QueueEmptyException("Queue is empty.");
+        }
+        return header.element;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public void clear() {
+        header = null;
+        trailer = null;
+        size = 0;
+    }
 
     private class ListNode {
         private T element;
